d9725659f7546424426a889c63a853e56e71ab55
Denis <protsdv@gmail.com>
2023-05-03 12:34:21 +0300

    Initial commit

---
+++ src/test/java/home/exercise/skypro_exercises_course4_hw2/SkyProExercisesCourse4Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package home.exercise.skypro_exercises_course4_hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProExercisesCourse4Hw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
